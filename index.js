372c94188fb64fd067d93aeceaf7e16c1e4223d3
lukeMadu <63106588+lukeMadu@users.noreply.github.com>
2020-04-03 14:08:33 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let name = "luke";
+let age = 15;
+let school = "code nation";
+console.log("hi, my name is " + name);
+//name = "jamie"
+//console.log("Hi, my name is " + name)
+console.log("i am " + age + " years old")
+console.log("i attend " + school)
+
+let numberOne;
+let numberTwo;
+let total;
+
+const addition = (numberOne, numberTwo) => {
+    total = numberOne + numberTwo;
+    console.log(total);
+};
+
+addition(20, 30);
+addition(69, 420);
+addition(40, 15);
